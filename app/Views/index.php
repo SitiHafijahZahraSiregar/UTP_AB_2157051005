c125489cdd809d2b33398e91ad5d421d755f18be
SitiHafijahZahraSiregar <sitihafizah579@gmail.com>
2023-10-17 18:04:51 +0700

    layouting

---
+++ app/Views/index.php
@@ -0,0 +1,7 @@
+
+
+<?= $this->extend('layouts/app'); ?>
+<?= $this->section('content'); ?>
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">UTP Web Lanjut</h1>
+<?= $this->endSection(); ?>
